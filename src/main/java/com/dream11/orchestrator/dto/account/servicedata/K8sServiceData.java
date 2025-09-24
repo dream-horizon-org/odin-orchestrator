16c06cc354087e733f17e0ecd9787cbc9463d152
Akshay Patidar <akshaypatidar1999@gmail.com>
2025-09-24 15:03:04 +0530

    chore: remove unused fields
    Signed-off-by: Akshay Patidar <akshaypatidar1999@gmail.com>

--- src/main/java/com/dream11/orchestrator/dto/account/servicedata/K8sServiceData.java
+++ src/main/java/com/dream11/orchestrator/dto/account/servicedata/K8sServiceData.java
@@ -4,9 +4,7 @@ import com.dream11.orchestrator.constants.NamespaceProviderType;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,22 +15,11 @@ import lombok.NoArgsConstructor;
 public class K8sServiceData {
   List<Cluster> clusters = new ArrayList<>();
 
-  String serviceIAMRole;
-
-  String pullSecrets;
-
-  Map<String, String> serviceAnnotations = new HashMap<>();
-
-  Map<String, String> serviceAccountAnnotations = new HashMap<>();
-
-  Map<String, String> environmentVariables = new HashMap<>();
-
   @Data
   public static class Cluster {
     String name;
     String kubeconfig; // Base64 encoded kube config
     NamespaceConfig namespaceConfig;
-    Map<String, List<String>> labels = new HashMap<>();
   }
 
   @Data
