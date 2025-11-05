ec19d67cd479ba28e96f7ef0c1453669f97f6ad0
SohithKumarPavuluri <sohith.pavuluri@dream11.com>
2025-11-05 19:04:07 +0530

    formatted all tests in AAA pattern

--- src/test/java/com/dream11/orchestrator/service/DiscoveryClientServiceTest.java
+++ src/test/java/com/dream11/orchestrator/service/DiscoveryClientServiceTest.java
@@ -82,8 +82,12 @@ class DiscoveryClientServiceTest {
   @Test
   void testContainsCnameRecordsWithIp() throws JsonProcessingException {
     // Arrange
+<<<<<<< HEAD
     String discoveryOutput = new JSONObject("key", "192.168.1.1").toString();
 
+=======
+    String discoveryOutput = new JSONObject().put("key", "192.168.1.1").toString();
+>>>>>>> cb7b4fa (formatted all tests in AAA pattern)
     // Act
     boolean result = discoveryClientService.containsCnameRecords(discoveryOutput);
 
